e517f624874695ab7c3176d7bc5bc227ab755806
Sabrina Markon <phpsitescripts@outlook.com>
2018-08-12 06:16:24 -0600

    Add route for showing all listings, mongo connection, start express server

--- app.js
+++ app.js
@@ -21,11 +21,61 @@ function errorHandler(err, req, res, next) {
 
 // connect to mongodb video database.
 MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+    
+    // make sure no error.
+    assert.equal(null, err);
+
+    console.log("Successfully connected to MongoDB.");
+
+    //////////////////////// route to main url listing the movies.
+    app.get('/', function(req, res) {
+        
+        // get the movies from the database.
+        db.collection('movies').find({}).toArray(function(err, docs) {
+
+            // check for problems.
+            if (err) { throw err; }
+            
+            // are there any movies at all?
+            if (docs.length < 1) {
+                return res.send('There are no movies here yet');
+            }
+
+            // there are movies, so send them to the movies.html template.
+            res.render('movies', {movies: docs});
+
+        });
+    });
+
+    //////////////////////// route to get to form to add a new movie.
+    app.get('/add', function(req, res) {
+
+        // load the add.html template. Don't need to pass variables now (simple version currently).
+        res.render('add');
+
+    });
+
+    //////////////////////// route to submit the form to add a new movie.
+    app.post('/add', function(req, res) {
+
+        //
+
+    });
+
+    //////////////////////// route not found.
+    app.use(function(req, res) {
+        res.sendStatus('404');
+    });
 
 
-
-
-
+    // Start up Express server.
+    const server = app.listen(3000, function() {
+        
+        // post
+        const port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+        
+    });
 
 });
 
